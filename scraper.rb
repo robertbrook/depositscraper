4f3938d2d3456e5ac2b9b97ee189552727fb75a5
Robert Brook <mail@robertbrook.com>
2008-12-31 11:45:56 +0000

    fighting with arrays

--- scraper.rb
+++ scraper.rb
@@ -1,16 +1,21 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-require 'enumerator'
+require "yaml"
+require "enumerator"
 
 # http://deposits.parliament.uk/
 
 doc = Nokogiri::HTML(open("deposits.html"))
 targets = Array.new
 
-(doc/"tr/td[@bgcolor='#e3e3e3']").each do |td|
-  targets << td.inner_html
+(doc/"tr").each do |tr|
+  targets << tr.inner_html
 end
-   
+
 targets.slice!(0..4)
-targets.each_slice(5) {|a| p a}
+targets.each_slice(4) {|a| y a}
+
+
+# 
+# targets.each_slice(5) {|a| p a}
