60f9aa1c659d3336968dd8f3cd6d8db3ee8d8177
Robert Brook <mail@robertbrook.com>
2008-12-31 14:02:04 +0000

    much better, but terrible code

--- scraper.rb
+++ scraper.rb
@@ -10,12 +10,17 @@ doc = Nokogiri::HTML(open("deposits.html"))
 targets = Array.new
 
 doc.css('tr').each do |tr|
-  targets << tr.content
+  targets << tr
 end
 
 targets.slice!(0..4)
-targets.each_slice(4) {|a| y a.slice(1..2)}
-
-
-# 
-# targets.each_slice(5) {|a| p a}
+targets.each_slice(4) {|a| 
+  b = a.slice!(1..2)
+  if b[0] then
+  b[0].css('td').each do |td|
+    p td.inner_html
+  end
+end
+  p b[1].content if b[1]
+  p ""
+  }
