c102388b26899bcfefa5578d4a33286a30363d0c
Robert Brook <mail@robertbrook.com>
2009-01-02 17:21:32 +0000

    tweaking tuples, debug code

--- scraper.py
+++ scraper.py
@@ -2,6 +2,18 @@
 import pprint
 import re
 
+class Deposit:
+  def __init__(self, aTuple):
+    self.number = aTuple[0][0]
+    self.legislature = aTuple[0][1]
+    self.date_deposited = aTuple[0][2]
+    self.department = aTuple[0][3]
+    self.text = aTuple[1]
+    self.download_link = aTuple[0][4]
+    self.download_text = aTuple[0][5]
+    self.associated_legislation = aTuple[2]
+
+
 valign = re.compile(' valign="top"><td bgcolor="#e3e3e3"><b>(.*)</b></td><td bgcolor="#e3e3e3"><b>(.*)</b></td><td bgcolor="#e3e3e3"><b>(.*)</b></td><td colspan="1" bgcolor="#e3e3e3" width="45%">(.*)</td><td bgcolor="#e3e3e3" width="25%" align="right"><A HREF="(.*)">(.*)</A></td></tr>')
 td_colspan = re.compile('<td colspan="2">(.*)</td>')
 td_i = re.compile('><td colspan="3" /><td><i>(.*)</i></td></tr>')
@@ -18,4 +30,10 @@ for line in deposits_html:
                 second = td_colspan.findall(e3e3e3[i+1])[0]
                 third = td_i.findall(e3e3e3[i+2])[0]
                 list_of_lists.append((first, second, third))
-            pprint.pprint(list_of_lists)
+                d = Deposit((first, second, third))
+                print d.legislature
+            # pprint.pprint(list_of_lists[4])
+            z = Deposit(list_of_lists[4])
+            
+            print z.text
+            
